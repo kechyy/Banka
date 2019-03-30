2f060e74c2293e1c105fc692e35f1bac597370dd
nkechi <kech3443@gmail.com>
2019-03-30 06:12:15 -0700

    [chore #165002071] dummy server setup

---
+++ app.js
@@ -0,0 +1,16 @@
+import express from 'express';
+const app = express();
+import bodyParser from 'body-parser';
+import userRoutes from './server/routes/userRoutes';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const port = process.env.PORT
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use('/api/v1/users',userRoutes);  
+
+app.listen(port,()=>{
+    console.log(`Server started on port ${port}...`);
+})
---
+++ server/data.js
@@ -0,0 +1,15 @@
+const userSignIn = [
+    {
+        id: 1,
+        firstName: 'Nkechi',
+        lastName: 'Ogbonna',
+        email: 'kech3443@gmail.com',
+        accountNumber: '000123467',
+        accountType: 'savings'
+    }
+];
+
+
+export  default userSignIn
+
+
