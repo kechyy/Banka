0d4aaf6ec9b3ca8bc3893ab4e014f5d38fa42a8f
nkechi <kech3443@gmail.com>
2019-04-13 10:58:32 -0700

    bug(Activate/Deactivate account bug fixed) Activate/Deactivate account travis flagged bug error fixed: [Starts #165330875]

--- server/tests/DeactivateActivateAccountController.js
+++ server/tests/DeactivateActivateAccountController.js
@@ -5,13 +5,16 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
 import { accountEmailExist, accountCreated } from './mockObjects/accountControllerObjectData';
+
 import app from '../../app';
 
 const { expect } = chai;
 chai.use(chaiHttp);
-const accountEndPoint = '/api/v1/account';
 const signUpEndPoint = '/api/v1/auth/signup';
+const accountUpdateEndPoint = '/api/v1/account/0004456511';
+const accountErrorUpdateEndPoint = '/api/v1/account/0004456222';
 let generateToken;
+
 describe('Create token for signup user', () => {
   it('should return token for successful login', (done) => {
     chai.request(app)
@@ -19,7 +22,7 @@ describe('Create token for signup user', () => {
       .send({ id: 1,
         firstName: 'Rosemary',
         lastName: 'Emmanuel',
-        email: 'rossy2@gmail.com',
+        email: 'rossy3@gmail.com',
         password: 'rosemary123&%',
         cpassword: 'rosemary123&%'
       })
@@ -30,25 +33,26 @@ describe('Create token for signup user', () => {
       });
   });
 });
-describe('Test for create account controller', () => {
-  it('should return status code 409 for account email already exist in dummy data', (done) => {
+
+describe('Test for deactivate and activate account controller', () => {
+  it('should return status code 200 for account successfully updated', (done) => {
     chai.request(app)
-      .post(accountEndPoint)
-      .send(accountEmailExist)
+      .patch(accountUpdateEndPoint)
+      .send({ accountNumber: '0004456511' })
       .set('authorization', generateToken)
       .end((error, response) => {
-        expect(response).to.have.status(409);
-        expect(response.body.error).to.equal('Account with this email already exist');
+        expect(response).to.have.status(200);
         done();
       });
   });
-  it('should return status code 201 for account successfully created', (done) => {
+  it('should return status code 404 for account number not found', (done) => {
     chai.request(app)
-      .post(accountEndPoint)
-      .send(accountCreated)
+      .patch(accountErrorUpdateEndPoint)
+      .send({ accountNumber: '0004456222' })
       .set('authorization', generateToken)
       .end((error, response) => {
-        expect(response).to.have.status(201);
+        expect(response).to.have.status(404);
+        expect(response.body.error).to.equal('Invalid account number');
         done();
       });
   });
