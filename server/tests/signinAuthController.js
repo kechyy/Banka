25ebf9b7748e918472fbfaae86f27cc7e8b60ff0
nkechi <kech3443@gmail.com>
2019-04-27 04:13:24 -0700

    bg(feedback updated): LFA feedback updated [Finishes #165654403]

---
+++ server/tests/signinAuthController.js
@@ -0,0 +1,36 @@
+/* eslint-disable object-curly-newline */
+/* eslint-disable no-undef */
+/* eslint-disable import/no-extraneous-dependencies */
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import { userConfirmed, confirmEmail, confirmPassword } from './mockObjects/signinControllerObjectData';
+
+import app from '../../app';
+
+const { expect } = chai;
+chai.use(chaiHttp);
+const signInEndPoint = '/api/v1/auth/signin';
+
+describe('Test for user signin controller', () => {
+  it('should return status code 400 for user email does not exist', async () => {
+    const response = await chai.request(app)
+      .post(signInEndPoint)
+      .send(confirmEmail);
+    expect(response).to.have.status(400);
+    expect(response.body.error).to.equal('User does  not exist. You need to signup');
+  });
+  it('should return status code 404 for user signin not in database', async () => {
+    const response = await chai.request(app)
+      .post(signInEndPoint)
+      .send(confirmPassword);
+    expect(response).to.have.status(404);
+    expect(response.body.error).to.equal('Please enter a valid username and password');
+  });
+  it('should return status code 200 for user signin successful', async () => {
+    const response = await chai.request(app)
+      .post(signInEndPoint)
+      .send(userConfirmed);
+    expect(response).to.have.status(200);
+    expect(response.body).to.be.a('object');
+  });
+});
